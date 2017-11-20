6cb753e71b7cfa50e0102e155d3cde63af86c190
bbattin <33416208+bbattin@users.noreply.github.com>
2017-11-20 11:32:16 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace _20171118HW1
+
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+
+            Week day;
+            string sDay;
+            
+            do
+            {
+                Console.Write("Day of the week number or name: ");
+                sDay = Console.ReadLine();
+                Enum.TryParse(sDay, out day);
+                Console.WriteLine("day = {0}, (int)day = {1}", day, (int)day);
+
+            } while (Enum.TryParse(sDay, out day) && (day < Week.LastDaysValue));
+
+            Console.WriteLine("Error input");
+
+            Console.ReadKey();
+        }
+    }
+}
