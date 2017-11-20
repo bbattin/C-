22f1ae35dc634b91dd5ae5841ef24243e4bb21ee
bbattin <33416208+bbattin@users.noreply.github.com>
2017-11-20 12:10:57 +0200

    Update 1118HW2.cs

--- 1118HW2.cs
+++ 1118HW2.cs
@@ -6,6 +6,20 @@ using System.Threading.Tasks;
 
 
 namespace _20171118HW2
+    
+     [Flags]
+        enum MultyDaysWeek : byte
+        {
+            NoDay = 0x00, 
+            Monday = 0x01,
+            Tuesday = 0x02,
+            Wednesday = 0x04,
+            Thursday = 0x08,
+            Friday = 0x10,
+            Saturday = 0x20,
+            Sunday = 0x40,
+            LastDaysValue = 0x80
+        }
 
 {
     class Program
