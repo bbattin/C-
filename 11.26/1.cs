051b8e2546f3b9f14c59282b0b8e007a86621c1e
bbattin <33416208+bbattin@users.noreply.github.com>
2017-11-27 12:12:13 +0200

    Update 1.cs

--- 11.26/1.cs
+++ 11.26/1.cs
@@ -28,9 +28,41 @@ namespace _20171126HW10
         
         //  функции
         
+        public static byte SwitchWeek(string numberDay)
+        {  
+            byte day = 0;
+            switch (numberDay)
+                {
+                    case "1":
+                        day = Monday;
+                        break;
+                    case "2":
+                        day = Tuesday;
+                        break;
+                    case "3":
+                        day = Wednesday;
+                        break;
+                    case "4":
+                        day = Thursday;
+                        break;
+                    case "5":
+                        day = Friday;
+                        break;
+                    case "6":
+                        day = Saturday;
+                        break;
+                    case "7":
+                        day = Sunday;
+                        break;
+                    default:
+                        break;
+                }
+        return day;
+        }
+        
         public static byte ScheduleRec()
         {
-            string numberDay;
+            string number;
             byte day1 = 0;
             byte day2 = 0;
             ushort day = 0;
@@ -38,77 +70,27 @@ namespace _20171126HW10
             {
                 day2 = (byte)(day1 | day2);
                 Console.Write("Day of the week number (mon-1, tue-2, wed-3, thu-4, fri-5, sat-6, sun-7, end-other char): ");
-                numberDay = Console.ReadLine();
-                switch (numberDay)
-                {
-                    case "1":
-                        day1 = Monday;
-                        break;
-                    case "2":
-                        day1 = Tuesday;
-                        break;
-                    case "3":
-                        day1 = Wednesday;
-                        break;
-                    case "4":
-                        day1 = Thursday;
-                        break;
-                    case "5":
-                        day1 = Friday;
-                        break;
-                    case "6":
-                        day1 = Saturday;
-                        break;
-                    case "7":
-                        day1 = Sunday;
-                        break;
-                    default:
-                        break;
-                }
+                number = Console.ReadLine();
+                day1 = SwitchWeek(number);
 
 
             } while (ushort.TryParse(numberDay, out day) && day > 0 && day < 8);
             Console.WriteLine("Thanks, schedule recorded.");
-            return day2;
+            return day2; 
         }
         
         
         public static byte ScheduleEdit(byte day2)
         {
-            string numberDay;
+            string number;
             byte day1 = 0;
             ushort day = 0;
             do
             {
                 day2 = (byte)(~day1 | day2);
                 Console.Write("Day of the week number exclude or add (end-other char): ");
-                numberDay = Console.ReadLine();
-                switch (numberDay)
-                {
-                    case "1":
-                        day1 = Monday;
-                        break;
-                    case "2":
-                        day1 = Tuesday;
-                        break;
-                    case "3":
-                        day1 = Wednesday;
-                        break;
-                    case "4":
-                        day1 = Thursday;
-                        break;
-                    case "5":
-                        day1 = Friday;
-                        break;
-                    case "6":
-                        day1 = Saturday;
-                        break;
-                    case "7":
-                        day1 = Sunday;
-                        break;
-                    default:
-                        break;
-                }
+                number = Console.ReadLine();
+                day1 = SwitchWeek(number);
                 
             }
             while (ushort.TryParse(numberDay, out day) && day > 0 && day < 8);
@@ -119,39 +101,15 @@ namespace _20171126HW10
         
         public static void Today(byte day2)
         {
-            string numberDay;
+            string number;
             byte day1 = 0;
             ushort day = 0;
             do
             {
                 Console.Write("Day of the week number today (end-other char): ");
                 numberDay = Console.ReadLine();
-                switch (numberDay)
-                {
-                    case "1":
-                        day1 = Monday;
-                        break;
-                    case "2":
-                        day1 = Tuesday;
-                        break;
-                    case "3":
-                        day1 = Wednesday;
-                        break;
-                    case "4":
-                        day1 = Thursday;
-                        break;
-                    case "5":
-                        day1 = Friday;
-                        break;
-                    case "6":
-                        day1 = Saturday;
-                        break;
-                    case "7":
-                        day1 = Sunday;
-                        break;
-                    default:
-                        break;
-                }
+                switch (number)
+                day1 = SwitchWeek(number);
                 if (ushort.TryParse(numberDay, out day) && day > 0 && day < 8)
                 {
                     if ((day1 & day2) == day1)
