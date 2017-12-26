d57eef9f1971acec9298b402c5a909e7bebf12e6
bbattin <33416208+bbattin@users.noreply.github.com>
2017-12-26 14:10:44 +0200

    Update UI.cs

--- 2017/12.25/UI.cs
+++ 2017/12.25/UI.cs
@@ -20,7 +20,7 @@ namespace _20171224HW
 
         public static void PrintGroup(Group p)
         {
-            Console.WriteLine(Environment.NewLine + "Group name: {0}, cpec: {1}, students: ", p.name, p.spec);
+            Console.WriteLine(Environment.NewLine + "Group name: {0}, specialty: {1}, students: ", p.name, p.spec);
             for (int i = 0; i < p.students.Length; i++)
             {
                 PrintStudent(p.students[i]);
@@ -29,7 +29,25 @@ namespace _20171224HW
         public static void PrintAver(Student s, int aver)
         {
            Console.WriteLine();
-           Console.Write(Environment.NewLine + "The average scores of students: {0} - {1}", s.name, aver);
+           Console.Write(Environment.NewLine + "Average score on the student: {0} - {1}", s.name, aver);
+        }
+
+        public static void PrintAverGr(Group g, int aver)
+        {
+            Console.WriteLine();
+            Console.Write(Environment.NewLine + "Average score for the group: {0} - {1}", g.name, aver);
+        }
+
+        public static void CaptionDelStInCopy()
+        {
+            Console.WriteLine();
+            Console.WriteLine(Environment.NewLine + "Delete the last student in the copy of the group:");
+        }
+
+        public static void CaptiontAddStInMain()
+        {
+            Console.WriteLine();
+            Console.WriteLine(Environment.NewLine + "Adding a student to the main group:");
         }
     }
 }
