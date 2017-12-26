c65e70d86a9d3e99cda8ef20c1200859c2337c5e
bbattin <33416208+bbattin@users.noreply.github.com>
2017-12-26 14:09:49 +0200

    Update Group.cs

--- 2017/12.25/Group.cs
+++ 2017/12.25/Group.cs
@@ -12,7 +12,7 @@ namespace _20171224HW
         public string spec;
         public Student[] students;  // reference type
 
-
+        // добавление студентов
         public Group(string Name, string Spec, Student [] Students)
         {
             this.name = Name;
@@ -20,34 +20,44 @@ namespace _20171224HW
             this.students = Students;
         }
 
+        // добавление студента
+        public static void Add(ref Group gr, Student newStudent)
+        {
+            Array.Resize(ref gr.students, gr.students.Length + 1);
+            gr.students[gr.students.Length - 1] = newStudent;
+        }
+
+        // удаление последнего студента
+        public static void Delete(ref Group gr)
+        {
+            Array.Resize(ref gr.students, gr.students.Length - 1);
+        }
+
         // полное копирование
         public static Group GetFullCopy(Group source)
         {
-            Group grCopy = source;    // копирование полей value types
-            // обработка reference types
-            grCopy.students = (Student[])source.students.Clone();
-
-
+            Group grCopy = source;                                  // копирование полей value types
+            grCopy.students = (Student[])source.students.Clone();   // обработка reference types
             return grCopy;
         }
 
-        // поиск средней оценки по каждому студенту
-        public static int Average(Student s)
+        // поиск средней оценки по всей группе
+        public static int AverageGroup(Group g)
         {
             int x = 0;
             int sum = 0;
-            int rezult = 0;
+            double rezult = 0;
+            int rez = 0;
 
-            for (int i = 0; i < s.assess.Length; i++)
+            for (int i = 0; i < g.students.Length; i++)
             {
-                x = s.assess[i];
+                x = Student.Average(g.students[i]);
                 sum += x;
             }
-            rezult = sum / s.assess.Length;
-            return rezult;
+            rezult = (double)sum / g.students.Length;
+            rez = Convert.ToInt32(Math.Round(rezult));
+            return rez;
         }
-
-        
 
     }
 }
