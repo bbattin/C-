876877fd435d3b267e2dbcf9d82539dfae371fcd
bbattin <33416208+bbattin@users.noreply.github.com>
2017-12-26 14:10:16 +0200

    Update Student.cs

--- 2017/12.25/Student.cs
+++ 2017/12.25/Student.cs
@@ -18,5 +18,24 @@ namespace _20171224HW
             this.number = Num;
             this.assess = Assess;
         }
+        
+        // поиск средней оценки по конкретному студенту
+        public static int Average(Student s)
+        {
+            int x = 0;
+            int sum = 0;
+            double rezult = 0;
+            int rez = 0;
+
+            for (int i = 0; i < s.assess.Length; i++)
+            {
+                x = s.assess[i];
+                sum += x;
+            }
+            rezult = (double)sum / s.assess.Length;
+            rez = Convert.ToInt32(Math.Round(rezult));
+            return rez;
+        }
+
     }
 }
