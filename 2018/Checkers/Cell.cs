a0e5bda744cabe38a430b3b2c6ad7b3ddd5e7894
bbattin <33416208+bbattin@users.noreply.github.com>
2018-01-06 11:02:15 +0200

    Create Cell.cs

---
+++ 2018/Checkers/Cell.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Checkers
+{
+    public class Cell
+    {
+        public CellColor Color;
+        public Figure Fig;
+        public int x, y;
+    }
+}
