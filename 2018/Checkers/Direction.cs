74b561eb5c6844d9a64d3e54cf26c731d66f7e3e
bbattin <33416208+bbattin@users.noreply.github.com>
2018-01-06 11:03:21 +0200

    Create Direction.cs

---
+++ 2018/Checkers/Direction.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Checkers
+{
+    public enum Direction
+    {
+        NoDirection = 0,
+        Left,
+        Right,
+        Up,
+        Down,
+        Enter,
+        EscExit,
+        Backspace,
+        LastDirectionValue
+    }
+}
