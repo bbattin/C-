5cf85e59184705dc5bb39ac71ad12880af94d845
bbattin <33416208+bbattin@users.noreply.github.com>
2018-01-06 11:04:25 +0200

    Create FigureState.cs

---
+++ 2018/Checkers/FigureState.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Checkers
+{
+    public enum FigureState
+    {
+        White,
+        WhiteQueen,
+        Black,
+        BlackQueen
+    }
+}
